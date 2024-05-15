e95f8e8cfc498f2dce29e8d93f062f1e366b33fa
Matt Seddon <mattseddon@hotmail.com>
2024-05-15 15:19:23 +1000

    remove malware

--- .d.ts
+++
@@ -1,5 +0,0 @@
-declare module "@/components/ui" {
-    export const Button: React.FC<any>;
-    export const Input: React.FC<any>;
-}
-
--- next.config.mjs
+++
@@ -1,5 +0,0 @@
-/** @type {import('next').NextConfig} */
-import input_fns from 'input-fns';
-const nextConfig = {};
-
-export default nextConfig;
